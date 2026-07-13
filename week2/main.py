f0ac5444a70124e303b89249ac72e33412a3d1a3
tuski0 <tuski0@naver.com>
2026-07-13 20:19:20 +0900

    기능 1, 2 추가

--- week2/main.py
+++ week2/main.py
@@ -3,14 +3,16 @@ import sys
 import pandas as pd
 import numpy as np
 
+
 df = None
+
+SPENDING_DATA_PATH = '../data/spending (1).csv'
 
 def load_data():
     global df
-    path = '../data/spending (1).csv'
 
-    if os.path.exists(path) :
-        df = pd.read_csv(path, encoding="utf-8-sig")
+    if os.path.exists(SPENDING_DATA_PATH) :
+        df = pd.read_csv(SPENDING_DATA_PATH, encoding="utf-8-sig")
         rows , cols = df.shape
         print(f'데이터 로드 완료: {rows}행 x {cols}열')
     else :
@@ -35,6 +37,38 @@ def parse_date() :
     df["month"] = df["date"].dt.month.astype('Int64')
     df["day"] = df["date"].dt.day.astype('Int64')
     
+def standardize_category() :
+    cleaned_list = []
+    standardize_count = 0
+    
+    allowed_category = ['식비', '교통', '쇼핑', '의료', '문화', '기타']
+    
+    for val in df['category'] : 
+        
+        if not isinstance(val, str) :
+           cleaned_list.append('기타')
+           standardize_count += 1
+           continue
+        
+        if val in allowed_category :
+            cleaned_list.append(val)
+        else :
+            cleaned_list.append('기타')
+            standardize_count += 1
+            
+    df['category'] = cleaned_list
+    
+    print(f'카테고리 표준화 : 변경 {standardize_count}건')
+    
+    
+            
+    
+            
+    
+            
+        
+    
+    
     
     
     
@@ -42,9 +76,11 @@ def parse_date() :
     
 
 if __name__ == '__main__' :
+    
     # 기능 1
     load_data()
     parse_date()
+    standardize_category()
     
     
 
